9697fef9c8c83d3676bed5497c23c5a77451c596
clouie87 <carol.louie@gmail.com>
2014-10-06 18:31:38 -0300

    with disconnect and some notes to my .js code

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io= require('socket.io')(http);
+var io= require('socket.io')(http); //after downloading the socket.io we need to state the var we initialize the (socket.io)
+//and pass it through the http object
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-	console.log('a user connected');
+io.on('connection', function(socket){ // then listen to the connection event for incoming sockets
+	console.log('a user connected'); // if there is a connection, log it to the console. 
+	socket.on('disconnect', function(){ // listen to people disconnection events for outgoing socket
+		console.log('user disconnected');// if there is a disconnection, log it to the console. 
 	}
 });
 
