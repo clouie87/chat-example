8d7107b52df75ac0b9d8bc81f33f46359f605425
clouie87 <carol.louie@gmail.com>
2014-10-08 12:49:56 -0300

    slowly starting to integrate my html files from last week and doing some styling

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-var app = require('express')();
+//resource: http://expressjs.com/4x/api.html#application
+/* to create an express application we need:
+var express = require('express');
+var app = express(); */
+
+var app = require('express')();// i think these lines 2 and 3 are summed up to one in this line. 
 var http = require('http').Server(app);
 var io= require('socket.io')(http); //after downloading the socket.io we need to state the var we initialize the (socket.io)
 //and pass it through the http object
+//var proxyaddr = require('proxy-addr');
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
+
+app.get('/', function (req, res){
+	res.send('Welcome');
+});
+
+/*app.set('trust proxy', function(ip){
+	if(ip === '127.0.0.1' || ip === '104.131.24.149') return true; //trusted IP
+	else return false;
+}); this sets up my trusted proxy but i need to figure out what that means for my ip... */
+
+app.use(function (req, res, next) {
+	console.log('Time: %d', Date.now());
+	next();
+});
+
+
 
 io.on('connection', function(socket){ // then listen to the connection event for incoming sockets
 	socket.on('chat message', function(msg){
@@ -17,6 +39,9 @@ io.on('connection', function(socket){ // then listen to the connection event for
 		console.log('user disconnected');// if there is a disconnection, log it to the console. 
 	});
 });
+
+/*proxyaddr(req, function(addr){ return addr === '127.0.0.1'})
+proxyaddr(req, function(addr, i){ return i< 1}); */
 
 http.listen(3000, function(){
 	console.log('listening on *:3000');
