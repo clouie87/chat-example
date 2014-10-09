b8c109d74d88b4260c01f2aec9cd17605387cc13
clouie87 <carol.louie@gmail.com>
2014-10-09 08:35:07 -0300

    the actual script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,10 @@
 
-var name=""; // this is a global variable all my functions below gets it :)
+var name = "";
 
-
-function join(){
-	name = prompt ("Hey new friend! What's your name?"); // this needs to equal the global variable to work in my later function 
-	//or i could have used my var join =  and then joim = name)
-}
-
-console.log("scriptloader")
-function mySubmit(){
-	if(name.length === 0){
-		alert("Please Join Us first!");
+function getName(){
+	name = prompt("Hey new friend! What's your name?");
+		if (name.length === 0){
+			alert("Please enter your name!");
 		return false;
 	}
-	else{ //i broke this part :) //*/
-	var message = document.getElementById("usermsg").value;
-		//
-	console.log("message is "+message);
-	document.getElementById("chatbox").appendChild(document.createTextNode(message));
-		//this goes to my 'Chatbox' on my html and will add (append) what is written in my messages to my chatbox
-	document.getElementById("chatbox").appendChild(document.createElement("br"));
-		//this goes adds a break below my message (it cannot append childs (or children) so we have to create the Break ourselve with a createElement)
-	document.getElementById("usermsg").value=""; //this means that the message gets cleared after it is sent
-	return false;// this lets me submit with enter and not have to use to submit button (must create html onsubmit=mySubmit())
-}
-}
+};
