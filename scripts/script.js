16855359eb9094584b3067b4ac56c06d2fcf970a
clouie87 <carol.louie@gmail.com>
2014-10-09 11:51:54 -0300

    adding a welcome

--- scripts/script.js
+++ scripts/script.js
@@ -8,3 +8,20 @@ function getName(){
 		return false;
 	}
 };
+
+var welcome = document.createTextNode(name).value;
+console.log("Welcome " + name);
+document.write("Welcome " + name);
+
+
+function messages(){
+	if(name === name){
+		function changeBgPurple(){
+			messages.bgColor = "purple";
+		}
+	}else{
+		function changeBgYellow(){
+			messages.bgColor = "yellow";
+		}
+	}
+};
