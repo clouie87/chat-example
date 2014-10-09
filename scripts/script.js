83f6c2d5140ebfba580182907bed0ca392c6459a
clouie87 <carol.louie@gmail.com>
2014-10-09 17:57:26 -0300

    Welcome Name

--- scripts/script.js
+++ scripts/script.js
@@ -3,25 +3,11 @@ var name = "";
 
 function getName(){
 	name = prompt("Hey new friend! What's your name?");
-		if (name.length === 0){
+		if (name.length === 0){// my first successful if statement!!!! I'm so stocked!! 
 			alert("Please enter your name!");
 		return false;
 	}
+	document.getElementById("welcome").appendChild(document.createTextNode(name)); //i needed to add this here so that it knows ??
+	console.log("Welcome" + name + "!");// this lets me see it on my console. 
 };
 
-var welcome = document.createTextNode(name).value;
-console.log("Welcome " + name);
-document.write("Welcome " + name);
-
-
-function messages(){
-	if(name === name){
-		function changeBgPurple(){
-			messages.bgColor = "purple";
-		}
-	}else{
-		function changeBgYellow(){
-			messages.bgColor = "yellow";
-		}
-	}
-};
